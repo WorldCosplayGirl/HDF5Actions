764fdfc64f4cab18e269c5aee717d0a486856cf0
WorldCosplayGirl <164516590@qq.com>
2021-09-17 09:54:56 +0800

    Update publishFile.ps1

--- .github/workflows/publishFile.ps1
+++ .github/workflows/publishFile.ps1
@@ -1,6 +1,7 @@
 function zipHDF5() {
+    cd ..
     Write-Host "ziping HDF5..."
-    Start-Process "C:/Program Files/7-zip/7z.exe" -Wait -ArgumentList 'a archive1.zip .\build\*'
+    Start-Process "C:/Program Files/7-zip/7z.exe" -Wait -ArgumentList 'a archive1.zip .\bin\*'
     ls
 }
 
